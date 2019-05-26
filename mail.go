b4337955d192b83afd3ff979ee433b9b5ae879cb
HuaPing2017 <1161276086@qq.com>
2019-05-26 16:37:03 +0800

    提交Email文件

---
+++ mail.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/smtp"
+	"os"
+	"strings"
+	"time"
+)
+
+func SendToMail(user, password, host, to, subject, body, mialtype string) error {
+	hp := strings.Split(host, ":")
+	auth := smtp.PlainAuth("", user, password, hp[0])
+	var content_type string
+	if mialtype == "html" {
+		content_type = "Content-Type : text/" + mialtype + "; charset=UTF-8"
+	} else {
+		content_type = "Content-Type : text/plain" + "; charset=UTF-8"
+	}
+	msg := []byte("To:" + to + "\r\nFrom:" + user + "<" + user + "\r\nSubject:" + subject + "\r\n" + content_type + "\r\n\r\n" + body)
+	send_to := strings.Split(to, ";")
+	err := smtp.SendMail(host, auth, user, send_to, msg)
+	return err
+}
+
+func readLine2Array(filename string) ([]string, error) {
+	resulut := make([]string, 0)
+	file, err := os.Open(filename)
+	//fmt.Println(file)
+	if err != nil {
+		return resulut, errors.New("打开文件失败")
+	}
+	defer file.Close()
+	bf := bufio.NewReader(file)
+	for {
+		line, isPrefix, err1 := bf.ReadLine()
+		if err1 != nil {
+			if err1 != io.EOF {
+				return resulut, errors.New("读行成功")
+			}
+			break
+		}
+		if isPrefix {
+			return resulut, errors.New("行太长")
+		}
+		str := string(line)
+		resulut = append(resulut, str)
+	}
+	return resulut, nil
+}
+
+func main() {
+	user := "1161276086@qq.com"
+	password := "kxmkljsnnhuqicib"
+	host := "smtp.qq.com:25"
+	//to := "jennifer9409@163.com"
+	subject := "使用Golang发送邮件"
+	//body := "<html><body><h3>测试发送Email</h3></body></html>"
+	//fmt.Print("发送邮件")
+
+	// err := SendToMail(user, password, host, to, subject, body, "html")
+	// if err != nil {
+	// 	fmt.Println("发送邮件失败")
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Println("发送邮件成功")
+	// }
+	sendTo, err := readLine2Array("send.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	content, err := ioutil.ReadFile("email.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	body := string(content)
+
+	for i := 0; i < len(sendTo); i++ {
+		to := sendTo[i]
+		fmt.Println("Send email to " + to)
+		err = SendToMail(user, password, host, to, subject, body, "html")
+		if err != nil {
+			fmt.Println("发送邮件失败")
+			fmt.Println(err)
+			i--
+			time.Sleep(600 * time.Second)
+		} else {
+			fmt.Println("发送邮件成功!")
+		}
+	}
+}
---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"net/smtp"
+	"strings"
+	"time"
+
+	"github.com/tealeg/xlsx"
+)
+
+const (
+	HOST        = "smtp.qq.com"
+	SERVER_ADDR = "smtp.qq.com:25"
+	USER        = "1161276086@qq.com"
+	PASSWORD    = "kxmkljsnnhuqicib"
+)
+
+type Email struct {
+	to       string "to"
+	subject  string "subject"
+	msg      string "msg"
+	mailtype string "html"
+}
+
+func NewEmail(to, subject, msg, mailtype string) *Email {
+	return &Email{to: to, subject: subject, msg: msg, mailtype: mailtype}
+}
+func SendEmail(email *Email) error {
+	auth := smtp.PlainAuth("", USER, PASSWORD, HOST)
+	sendTo := strings.Split(email.to, ";")
+	done := make(chan error, 1024)
+	var content_type string
+	if email.mailtype == "html" {
+		content_type = "Content-Type: text/" + email.mailtype + "; charset=UTF-8"
+	} else {
+		content_type = "Content-Type: text/plain" + "; charset=UTF-8"
+	}
+
+	go func() {
+		defer close(done)
+		for _, v := range sendTo {
+
+			str := strings.Replace("From: "+USER+"~To: "+v+"~Subject: "+email.subject+"~"+content_type+"~~", "~", "\r\n", -1) + email.msg
+
+			err := smtp.SendMail(
+				SERVER_ADDR,
+				auth,
+				USER,
+				[]string{v},
+				[]byte(str),
+			)
+			done <- err
+		}
+	}()
+
+	for i := 0; i < len(sendTo); i++ {
+		<-done
+	}
+
+	return nil
+}
+func main() {
+	excelFileName := "foo.xlsx"
+	xlFile, err := xlsx.OpenFile(excelFileName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, sheet := range xlFile.Sheets {
+		for _, row := range sheet.Rows {
+			emailStr := &Email{
+				to:       row.Cells[1].String(),
+				subject:  "面试邀请",
+				msg:      "<html><body><h3>" + row.Cells[0].String() + ":</h3><h4>" + row.Cells[2].String() + "</h4></body></html>",
+				mailtype: "html",
+			}
+			err = SendEmail(emailStr)
+			if err != nil {
+				fmt.Println("给" + row.Cells[0].String() + "发送邮件失败!")
+				fmt.Println(err)
+				time.Sleep(600 * time.Second)
+			} else {
+				fmt.Println("给" + row.Cells[0].String() + "发送邮件成功!")
+			}
+		}
+	}
+}
